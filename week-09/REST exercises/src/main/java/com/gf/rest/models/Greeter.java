32b97df639c1c8eb918f92c5bc9baed0d221e7b3
Andras Madi-Szabo <madiszabo@gmail.com>
2020-07-21 07:49:56 +0200

    Greeter class added

--- week-09/REST exercises/src/main/java/com/gf/rest/models/Greeter.java
+++ week-09/REST exercises/src/main/java/com/gf/rest/models/Greeter.java
@@ -1,4 +1,15 @@
 package com.gf.rest.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class Greeter {
+
+  private String welcomeMessage;
+  private String name;
+  private String title;
+
 }
